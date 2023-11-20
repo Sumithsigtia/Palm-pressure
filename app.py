58721552b1031e05fb3273d4b001747dbb3e1b5f
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2023-11-20 11:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import joblib
+import pickle
 
-model = joblib.load('gradient_boosting_model (1).joblib')
+model = pickle.load(open('gradient_boosting_model.sav', 'rb'))
 
 
 # Streamlit App
